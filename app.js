08cfe26fe97b1bc7dd9d5afeaf78c671557e13db
Aditya Kumar <adityapal1409@gmail.com>
2023-08-11 17:31:12 +0530

    core modules

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const index = require("./first/index.js");
-console.log(index);
+console.log(index.func());
+
+//core modules
+
+const fs = require("fs");
+fs.writeFileSync("intro.txt", "This is the intro of the node");
