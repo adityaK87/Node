9ccfe4654dd5f8426e4349947542439be79cd01c
Aditya Kumar <adityapal1409@gmail.com>
2023-08-19 23:29:35 +0530

    modify app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const index = require("./first/index.js");
 console.log(index.func());
 
 //core modules
-
+//built-in modules
 const fs = require("fs");
 fs.writeFileSync("intro.txt", "This is the intro of the node");
+
+const data = require("./example.js");
+console.log(data);
---
+++ example.js
@@ -0,0 +1,7 @@
+module.exports.arr = ["item1", "item2"];
+
+const obj = {
+	person: "john cena",
+};
+
+module.exports.obj = obj;
