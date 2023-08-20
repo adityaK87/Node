3f6a9216a0490df62e9aeb4697d7273b9a32e1d3
Aditya Kumar <adityapal1409@gmail.com>
2023-08-20 16:18:54 +0530

    add comments

--- fsExample.js
+++ fsExample.js
@@ -1,33 +1,38 @@
 const fs = require("fs");
 
-// fs.readFile("intro.txt", "utf-8", (err, data) => {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log(data);
-// 	}
-// });
+// Reading file asynchronously
+fs.readFile("intro.txt", "utf-8", (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+});
 
-// const fileContent = fs.readFileSync("intro.txt", "utf-8");
-// console.log(fileContent);
+// Reading file Synchronously
+const fileContent = fs.readFileSync("intro.txt", "utf-8");
+console.log(fileContent);
 
-// fs.writeFile("example.txt", "Welcome to node js class", (err, data) => {
-// 	if (err) {
-// 		console.log(err);
-// 		return;
-// 	} else {
-// 		console.log(data);
-// 	}
-// });
+//Create fite
+fs.writeFile("example.txt", "Welcome to node js class", (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	} else {
+		console.log(data);
+	}
+});
 
-// fs.mkdir("new_Dir", (err) => {
-// 	if (err) {
-// 		console.log(err);
-// 		return;
-// 	}
-// 	console.log("Directory created");
-// });
+// Making new Directory
+fs.mkdir("new_Dir", (err) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log("Directory created");
+});
 
+// remove directory
 fs.rmdir("new_Dir", (err) => {
 	if (err) {
 		console.log(err);
